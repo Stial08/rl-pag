ee9b8673bb6024b87c5c54d0c0e7a1518124de48
Stiffen Alejandro Macias Quintero <stiffen.macias@tangosource.com>
2013-10-17 10:17:45 -0500

    Moved all js libraries

---
+++ db/migrate/20131016224902_add_field_for_users.rb
@@ -0,0 +1,9 @@
+class AddFieldForUsers < ActiveRecord::Migration
+  def change
+    add_column :user, :name, :text 
+    add_column :user, 
+    add_column :user,
+    add_column :user,
+    add_column :user,
+  end
+end
